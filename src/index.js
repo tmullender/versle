2fbf2fe78ef9d3b6e1b9835b9c6f8c476ef3a22f
Tim Mullender <t.j.mullender@gmail.com>
2022-03-17 09:11:19 +0000

    Adding NET copyright statement

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <div className={"footer"}>The Scriptures quoted are from the NET Bible®
+        <a href={"http://netbible.com"}>http://netbible.com</a>
+        copyright ©1996, 2019 used with permission from Biblical Studies Press, L.L.C. All rights reserved
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
