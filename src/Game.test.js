bfa0b1b91c4f326ec79f6c9e0683e32cc5dab359
Tim Mullender <t.j.mullender@gmail.com>
2022-03-17 16:01:03 +0000

    Fixing tests

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor, within} from '@testing-library/react';
 import Game from './Game';
 
-test('renders learn react link', () => {
-  render(<Game />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeEach(() => {
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve([{text: "A verse from the bible", bookname: "Book", chapter: "1", verse: "2"}])
+    })
+  );
+})
+
+test('renders the words', async () => {
+  await render(<Game />);
+  await waitFor(() => {
+      expect(screen.getByText(/A/)).toBeInTheDocument();
+      expect(screen.getByText(/bible/)).toBeInTheDocument();
+      expect(screen.getByText(/from/)).toBeInTheDocument();
+      expect(screen.getByText(/the/)).toBeInTheDocument();
+      expect(screen.getByText(/verse/)).toBeInTheDocument();
+  });
 });
