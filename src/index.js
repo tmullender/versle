822fe53281c71d15de1a6cf64009c4c7d8ceadf4
Tim Mullender <t.j.mullender@gmail.com>
2022-03-17 09:26:18 +0000

    Adding Wordle attribution

--- src/index.js
+++ src/index.js
@@ -6,9 +6,12 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <div className={"footer"}>The Scriptures quoted are from the NET Bible®
-        <a href={"http://netbible.com"}>http://netbible.com</a>
-        copyright ©1996, 2019 used with permission from Biblical Studies Press, L.L.C. All rights reserved
+    <div className={"footer"}>
+        <div>The Scriptures quoted are from the NET Bible®
+            <a href={"http://netbible.com"}>http://netbible.com</a>
+            copyright ©1996, 2019 used with permission from Biblical Studies Press, L.L.C. All rights reserved
+        </div>
+        <div>Inspired by <a href={"https://www.nytimes.com/games/wordle/"}>Wordle</a></div>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
